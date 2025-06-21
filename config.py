ede2f10884e1019af6d94fa7060e9e2f2c3fe937
homefixerkhulna <homefixerkhulna@gmail.com>
2025-06-21 19:59:34 +0600

    New add

---
+++ config.py
@@ -0,0 +1,6 @@
+# DO NOT COMMIT THIS FILE WITH SENSITIVE DATA IN PUBLIC REPOS
+# This file should be ignored using .gitignore
+
+VERIFY_TOKEN = "your_custom_verify_token"
+PAGE_ACCESS_TOKEN = "your_facebook_page_access_token"
+GEMINI_API_KEY = "your_gemini_api_key"
