b43ee8b73850f0d438dea3fc8b25e214eb48e77b
homefixerkhulna <homefixerkhulna@gmail.com>
2025-06-21 20:10:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # DO NOT COMMIT THIS FILE WITH SENSITIVE DATA IN PUBLIC REPOS
 # This file should be ignored using .gitignore
 
-VERIFY_TOKEN = "your_custom_verify_token"
-PAGE_ACCESS_TOKEN = "your_facebook_page_access_token"
-GEMINI_API_KEY = "your_gemini_api_key"
+VERIFY_TOKEN = "homefixer_messenger_bot_1234"
+PAGE_ACCESS_TOKEN = "24210754871938109|t80vrfm5DO5UR73O0wJ-FRCfe2c"
+GEMINI_API_KEY = "AIzaSyDeHev5U-8pDvG6dY_pF9ivSjhbNdMVuu4"
