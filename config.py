40b2255b4abe9d596512d57a3c4c0e6c6923af4e
homefixerkhulna <homefixerkhulna@gmail.com>
2025-06-21 19:27:38 +0600

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# DO NOT COMMIT THIS FILE WITH SENSITIVE DATA IN PUBLIC REPOS
-# This file should be ignored using .gitignore
-
-VERIFY_TOKEN = "homefixer_messenger_bot_1234"
-PAGE_ACCESS_TOKEN = "your_facebook_page_access_token"
-GEMINI_API_KEY = "your_gemini_api_key"
