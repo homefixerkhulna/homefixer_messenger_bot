f6f747dc6031917b682db264285bb17cacbc453f
homefixerkhulna <homefixerkhulna@gmail.com>
2025-06-22 16:25:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,8 @@ def send_message(recipient_id, message_text):
     }
     headers = {"Content-Type": "application/json"}
     requests.post(url, json=payload, headers=headers)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
