6695933b0acc210ace828a9d3fab89f0712bf1a1
homefixerkhulna <homefixerkhulna@gmail.com>
2025-09-28 12:51:51 +0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "api")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "your-verify-token")
 PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "your-page-access-token")
 
