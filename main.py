64ef2d9e8baddeb8c371b183c1992cb5eedf30db
homefixerkhulna <homefixerkhulna@gmail.com>
2025-10-31 06:10:17 +0600

    Update main.py1

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_reply(user_message):
                 return item.get(f"answer_{lang}", item.get("answer_en"))
 
     # Check for root JSON keys (another form of intent)
-    root_keys = [ "hi","tagline", "page_name", "description_bn", "description_en"]
+    root_keys = [ "hi", "AC cleaning price", "এসি পরিষ্কারের মূল্য", "tagline", "page_name", "description_bn", "description_en"]
     for key in root_keys:
         if key in msg_lower:
             if lang == "bn" and key.endswith("_bn"):
